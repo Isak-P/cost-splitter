01667b78c038bb0f39b22a79da86d0dc651f7d89
Isak-P <isakp@kth.se>
2020-12-07 20:05:21 +0100

    Form now returns data to app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Form from "./Form";
 
+interface form {
+
+}
+
 function App() {
+
+  const [data, setData] = useState([]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <Form />
+        <p>Fill in everyone's contributions</p>
+        <Form onComplete={(data:any) => setData(data)} />
       </header>
     </div>
   );
